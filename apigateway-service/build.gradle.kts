2357250e162bb1ed7583607fc01a848f917e096f
rugii913 <rugii913@gmail.com>
2024-10-12 20:58:52 +0900

    chore: 종속성 변경-spring-cloud-starter-gateway 사용
    종속성 변경
    - 기존에 사용하던 종속성 주석 처리
      - spring-cloud-starter-gateway-mvc
      - spring-webmvc
    - 새 종속성 추가
      - org.springframework.cloud:spring-cloud-starter-gateway
    - 강의와 같은 방식으로 Spring Cloud Routing의 Reactive Gateway를 사용하도록 변경함
      - cf. Spring Initialzr(https://start.spring.io/) 기준, Reactive Gateway를 추가하면 testImplementation("io.projectreactor:reactor-test")도 추가됨

--- apigateway-service/build.gradle.kts
+++ apigateway-service/build.gradle.kts
@@ -26,12 +26,16 @@ repositories {
 extra["springCloudVersion"] = "2023.0.3"
 
 dependencies {
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
+    // implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc") // spring-cloud-starter-gateway(reactive gateway)를 사용하면서 주석 처리
+    // implementation("org.springframework:spring-webmvc") // spring-cloud-starter-gateway(reactive gateway)를 사용하면서 주석 처리
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-    implementation("org.springframework:spring-webmvc")
+
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
