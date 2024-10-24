2beac3e03cc17dbb4eb186eacbdd8ffe1be6112e
rugii913 <rugii913@gmail.com>
2024-10-24 13:12:43 +0900

    chore: (별도 진행)Gradle 멀티 프로젝트를 사용한 구조로 변경
    (별도 진행) Gradle 멀티 프로젝트를 사용한 구조로 변경
    - 여러 IDE를 실행하여 프로젝트를 관리하기 귀찮음이 있기에 Gradle의 멀티 모듈을 사용하도록 변경

---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+// root project의 name 지정
+rootProject.name = "msa-application-with-spring-cloud"
+
+// root project가 포함할 sub project의 경로들 - sub project의 이름은 기본적으로는 path와 같게 지정됨
+include("apigateway-service", "discovery-service", "first-service", "second-service", "user-service")
