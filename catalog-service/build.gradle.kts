4aacc328a5615e044cc3161bfcc2ad5186d84c82
rugii913 <rugii913@gmail.com>
2024-10-31 22:55:14 +0900

    init: catalog-service 시작
    - subproject로 catalog-service 시작
    - 시작 시 종속성
      - spring-cloud-starter-netflix-eureka-client
      - spring-boot-starter-web
      - spring-boot-starter-validation
      - spring-boot-starter-data-jpa
      - h2
      - devtools
      - lombok

---
+++ catalog-service/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.3"
+
+dependencies {
+    // spring-cloud-starter-netflix-eureka-client
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+
+    // spring-boot-starter-web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // spring-boot-starter-validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // spring-boot-starter-data-jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // h2
+    runtimeOnly("com.h2database:h2:2.2.224")
+
+    // devtools
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
