c2479865db3e3675225ea76552f22b93c61ce6bf
rugii913 <rugii913@gmail.com>
2024-11-15 18:05:43 +0900

    feat: apigateway-service에서 config server 사용하기 및 actuator refresh 활용 등
    - apigateway-service에서 config server 사용하기
      - config server를 사용하기 위해 bootstrap.yml에 사용할 config server의 uri와 설정 이름 명시
    - actuator refresh를 이용해 설정 정보 불러오기
      - application.yml에 노출할 actuator endpoint 명시
    - actuator의 httpexchanges 활용하여 http 요청 응답 확인하기
      - HttpExchangeRepository 빈 등록

--- apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,7 +1,10 @@
 package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
@@ -10,4 +13,8 @@ public class ApigatewayServiceApplication {
         SpringApplication.run(ApigatewayServiceApplication.class, args);
     }
 
+    @Bean // actuator의 httpexchanges를 확인하기 위해 저장소를 연결할 bean을 등록 (cf. Spring Boot 3.x 이후로 httptrace에서 이름 변경)
+    public HttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
 }
